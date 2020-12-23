f7acbb06e03d5dbea442e28750598a90318bc24d
suravitpop <pop.suravit29@gmail.com>
2020-12-24 02:47:53 +0700

    test git push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule,Validators } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'angular-pretest';
+
+export class AppComponent{
+  numcheck = new FormControl('');
+  numcheckform;
+  Numchecker: Array<Object> = [
+    { id: 0, name: "isPrime" },
+    { id: 1, name: "IsFibonacci" }
+  ];
+
+  selected() {
+    let num = this.numcheck.value;
+
+    function isPrime(num) {
+      for (var i = 2; i < num; i++)
+        if (num % i === 0) return false;
+      return num > 1;
+    }
+    
+    function isFibonacci(num) {
+      var fib,
+        a = (5 * Math.pow(num, 2) + 4),
+        b = (5 * Math.pow(num, 2) - 4)
+
+      var result = Math.sqrt(a) % 1 == 0,
+        res = Math.sqrt(b) % 1 == 0;
+
+      //fixed this line
+      if (result || res == true) // checks the given input is fibonacci series
+      {
+        return true;
+
+      } else {
+        return false;
+      }
+    }
+
+    if (this.numcheckform.name == "isPrime") {
+      return isPrime(num);
+      } else {
+       return isFibonacci(num);
+      }
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { CommonModule } from '@angular/common';
+import {NgModule,} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResizableModule } from './resizable/resizable.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports: [BrowserModule, CommonModule, ResizableModule, ReactiveFormsModule, NoopAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
